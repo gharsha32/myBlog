d7e4374a8250999708634ac9afadbd677a9afba2
Harsha Gangavarapu <harshagangavarapu32@gmail.com>
2019-01-14 11:10:41 +0530

    add script

---
+++ myBlog/script.js
@@ -0,0 +1,4 @@
+const link  = document.getElementById('link');
+link.addEventListener('click', () =>{
+document.write("<p>Hello everyone. Welcome to javascript. I am Harshavardhan and here you are going to learn concepts of javaScript. Ok. Let's get stright into the topic. some of the basic concepts in javaScript are:<ol><li>Document.write method</li><li>Console.log method</li><li>Alert method</li><li>For loops</li><li>While loops</li><li>Arrays</li><li>Objects</li><li>String concatination</li></ol></p>");
+});
